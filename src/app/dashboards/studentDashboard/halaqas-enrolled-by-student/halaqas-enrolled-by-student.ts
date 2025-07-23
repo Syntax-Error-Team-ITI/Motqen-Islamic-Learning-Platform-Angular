79c50d82cd27e266de0774c3143b0cd7a2f16f07
NehalKhalid <nehalkha92@gmail.com>
2025-07-23 06:47:17 +0200

    add rest of dashboard pages

---
+++ src/app/dashboards/studentDashboard/halaqas-enrolled-by-student/halaqas-enrolled-by-student.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-halaqas-enrolled-by-student',
+  imports: [],
+  templateUrl: './halaqas-enrolled-by-student.html',
+  styleUrl: './halaqas-enrolled-by-student.css',
+})
+export class HalaqasEnrolledByStudent {}
