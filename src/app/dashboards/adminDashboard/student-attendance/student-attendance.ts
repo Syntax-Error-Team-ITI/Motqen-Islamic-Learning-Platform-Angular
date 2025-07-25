8c234a169ace1f6646f48ff967cb1105a426c436
7egz <mustafareda3210@gmail.com>
2025-07-25 23:50:55 +0300

    Admin dashboard can show attendance of students in specific halaqa and at specific date

--- src/app/dashboards/adminDashboard/student-attendance/student-attendance.ts
+++ src/app/dashboards/adminDashboard/student-attendance/student-attendance.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HalaqaService } from '../../../services/halaqa-service';
+import { ProgressTrackingService } from '../../../services/progress-tracking-service';
+import { IHalaqaNamesList } from '../../../models/Halaqaa/ihalaqa-names-list';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-student-attendance',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './student-attendance.html',
   styleUrl: './student-attendance.css',
 })
-export class StudentAttendance {}
+export class StudentAttendance implements OnInit {
+  halaqaNamesList: IHalaqaNamesList[] = [];
+  selectedHalaqaId: number | null = null;
+  selectedHalaqaName: string = '';
+  selectedDate: string = '';
+  attendanceList: any[] = [];
+  attendanceLoaded: boolean = false;
+  loading: boolean = false;
+
+  constructor(
+    private halaqaService: HalaqaService,
+    private progressTrackingService: ProgressTrackingService
+  ) {}
+
+  ngOnInit(): void {
+    this.halaqaService.getHalaqaNamesList().subscribe({
+      next: (list) => {
+        this.halaqaNamesList = list;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  onHalaqaChange(event: Event) {
+    this.selectedHalaqaName = this.halaqaNamesList.find(
+      (halaqa) => halaqa.id === this.selectedHalaqaId
+    )?.name || '';
+  }
+
+  onLoadAttendance() {
+    if (!this.selectedHalaqaId || !this.selectedDate) return;
+    this.loading = true;
+    this.attendanceLoaded = false;
+    this.progressTrackingService
+      .getStudentAttendance(this.selectedHalaqaId, this.selectedDate)
+      .subscribe({
+        next: (data) => {
+          this.attendanceList = data;
+          this.attendanceLoaded = true;
+          this.loading = false;
+        },
+        error: (err) => {
+          this.attendanceList = [];
+          this.attendanceLoaded = true;
+          this.loading = false;
+          console.error(err);
+        },
+      });
+  }
+}
