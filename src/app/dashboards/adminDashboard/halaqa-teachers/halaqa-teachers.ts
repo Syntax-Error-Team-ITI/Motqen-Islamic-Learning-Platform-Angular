9112d74df392e08dfb848b6b2a9dfc5ee83c10eb
NehalKhalid <nehalkha92@gmail.com>
2025-07-26 21:09:13 +0200

    add halaqa Teachers page

---
+++ src/app/dashboards/adminDashboard/halaqa-teachers/halaqa-teachers.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-halaqa-teachers',
+  imports: [],
+  templateUrl: './halaqa-teachers.html',
+  styleUrl: './halaqa-teachers.css',
+})
+export class HalaqaTeachers {}
